87ac87ab70b6b34cead7000256ff78e02c0ebf81
SADEK HOSSAIN <shadeks16@gmail.com>
2022-03-23 18:37:51 +0600

    new added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,10 +2,25 @@ import React from 'react';
 import './Cart.css';
 const Cart = (props) => {
     const {cart} = props;
+    
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for(const product of cart){
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
+        shipping= shipping + product.shipping;
+    };
+    const tax = (total * 0.1).toFixed(2);
+    const grandTotal = total + shipping + parseFloat(tax);
     return (
-        <div>
+        <div className='cart'>
             <h3>Order Summery</h3>
-            <p>Selected Product: {cart.length}</p>
+            <p>Selected Product: {quantity}</p>
+            <p>Total Price:${total}</p>
+            <p>Total Shipping Charge: ${shipping}</p>
+            <p>Tax:${tax} </p>
+            <h4>Grand Total:${grandTotal}</h4>
         </div>
     );
 };
